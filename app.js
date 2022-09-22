50eaca8a6a9de4259969f8b19f793e5904ecc0cb
ruchikxxii <ruchik710@gmail.com>
2022-09-22 14:46:09 +0530

    added basic outline

--- app.js
+++ app.js
@@ -7,8 +7,24 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.set('view engine','ejs')
 
+const messages = [
+    {
+        username:"Bot",
+        content:"Welcome to ReChat"
+    }
+]
+
 app.get('/',(req,res)=>{
     res.sendFile(__dirname+"/public/login.html")
+})
+
+app.post('/login',(req,res)=>{
+    const username = req.body.username;
+    res.redirect(`/home?username=${username}`)
+})
+
+app.get('/home',(req,res)=>{
+    res.render('home',{username:req.query.username,messages:messages});
 })
 
 app.listen(3000,()=>{
